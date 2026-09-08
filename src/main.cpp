edda7cad1f10f6d24f52fa29b4515ee2984f4c9d
Ilya Naumov <ilya.naumov08@yandex.ru>
2026-09-08 19:10:06 +0300

    test 0.2

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream> 
 int main() { 
-    std::cout << "Hello, my name is Ilya. I am from IU5-12/4! Massege UPDATE" << std::endl; 
+    std::cout << "Hello, my name is Ilya. I am from IU5-12/4! Massege UPDATEE" << std::endl; 
     return 0; 
 }
